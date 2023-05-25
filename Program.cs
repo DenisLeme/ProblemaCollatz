dd1cb865870645ae4543b9dcfe4eef6a2215f107
Denis Leme <denis_leme@icloud.com>
2023-05-25 00:20:35 -0300

    Collatz Resolvido

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ProblemaCollatz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro positivo: ");
+            int numeroInicial = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Sequência para o número {numeroInicial}:");
+            MostrarSequenciaCollatz(numeroInicial);
+        }
+
+        static void MostrarSequenciaCollatz(int numero)
+        {
+            Console.Write($"{numero} ");
+
+            while (numero != 1)
+            {
+                if (numero % 2 == 0)
+                    numero /= 2;
+                else
+                    numero = (3 * numero) + 1;
+
+                Console.Write($"{numero} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
